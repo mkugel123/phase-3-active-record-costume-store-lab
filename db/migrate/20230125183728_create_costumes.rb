f7477ba3eaf4e599bab95d102390d1644fd18613
Zalman <moishkugel@gmail.com>
2023-01-25 13:55:44 -0500

    commit tree 9da3dfd82164f874636900f47735d005da944750

---
+++ db/migrate/20230125183728_create_costumes.rb
@@ -0,0 +1,11 @@
+class CreateCostumes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :image_url
+      t.string :size
+      t.timestamps
+    end
+  end
+end
